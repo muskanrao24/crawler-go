3740f242250319c8de8cdbce2e7f902e7f1a61a5
muskanrao24 <raomuskan1202@gmail.com>
2024-08-07 18:12:56 +0530

    Added simple parser, engine, fetcher

---
+++ crawler/dating/parser/cityList_test.go
@@ -0,0 +1,18 @@
+package parser
+
+import (
+	"distributed-web-crawler/crawler/fetcher"
+	"fmt"
+	"testing"
+)
+
+func TestParseCityList(t *testing.T) {
+	contents, err := fetcher.Fetch("http://www.zhenai.com/zhenghun")
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("%s\n", contents)
+
+	ParseCityList(contents)
+}
---
+++ crawler/fetcher/fetcher.go
@@ -0,0 +1,48 @@
+package fetcher
+
+import (
+	"bufio"
+	"fmt"
+	"golang.org/x/net/html/charset"
+	"golang.org/x/text/encoding"
+	"golang.org/x/text/encoding/unicode"
+	"golang.org/x/text/transform"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+const byteLength = 1024
+
+// fetch content from url
+func Fetch(url string) ([]byte, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("wrong status code: %d", resp.StatusCode)
+	}
+
+	bodyReader := bufio.NewReader(resp.Body)
+	e := determineEncoding(bodyReader)
+	utf8Reader := transform.NewReader(resp.Body, e.NewDecoder())
+
+	return ioutil.ReadAll(utf8Reader)
+}
+
+// determine the encoding of input stream
+func determineEncoding(r *bufio.Reader) encoding.Encoding {
+	// log.Printf("检测输入流的编码")
+	bytes, err := r.Peek(byteLength)
+
+	if err != nil {
+		log.Printf("Fetcher error: %v", err)
+		return unicode.UTF8
+	}
+	e, _, _ := charset.DetermineEncoding(bytes, "")
+	// log.Printf("编码为: %s", e)
+	return e
+}
